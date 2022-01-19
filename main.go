aa71ff46e1af92ea1b9856980a9822dd2dc84fb5
Haneul Lee <39932141+le2sky@users.noreply.github.com>
2022-01-19 14:06:21 +0000

    [theory]packages and imports

--- main.go
+++ main.go
@@ -4,12 +4,18 @@
 //fmt => 포맷팅하는 것으로 추측
 //go run main.go => 실행
 
+//2. Packages and imports
+//export function은 대문자로 시작함
+//소문자로 시작하면 private임
+
 package main
 
 import (
 	"fmt"
+	"main/something"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+  something.SayHello()
 }
