165ae6c4e68b204fc0970705d98413216bd58c65
Haneul Lee <39932141+le2sky@users.noreply.github.com>
2022-01-20 00:43:50 +0000

    [theory] const and var

--- main.go
+++ main.go
@@ -8,6 +8,12 @@
 //export function은 대문자로 시작함
 //소문자로 시작하면 private임
 
+//3. 변수와 상수
+// 상수 : const name type = "leesky"
+// 변수 : var name type = ""
+//   -> name:="leesky" <= 동적 타입언어처럼 타입을 지정해준다.
+//   -> 위와 같은 축약 선언은 함수내부에서만 작동가능
+//   -> 변수에만 적용가능하다
 package main
 
 import (
@@ -18,4 +24,7 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
   something.SayHello()
+  const name string = "leesky";
+
+
 }
