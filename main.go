6602d6ff36ba1d254c3e8a32add2d36fc7991286
Haneul Lee <39932141+le2sky@users.noreply.github.com>
2022-02-03 10:44:35 +0000

    [project/urlChecker] using goroutines and channels

--- main.go
+++ main.go
@@ -1,19 +1,57 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"main/mydict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request failed")
+
+type requestResult struct {
+  url string
+  status string
+}
+
 func main(){
-  dictionary := mydict.Dictionary{}
-  baseWord := "hello"
-  dictionary.Add(baseWord, "First")
-  err := dictionary.Update(baseWord, "Second")
-  if err != nil {
-    fmt.Println(err)
+   results := map[string]string{}
+   c := make(chan requestResult)
+
+   urls := []string{
+     "https://www.airbnb.com/",
+     "https://www.google.com/",
+     "https://www.amazon.com/",
+     "https://www.reddit.com/",
+     "https://soundcloud.com/",
+     "https://www.facebook.com/",
+     "https://www.instagram.com/",
+   }
+
+  for _,url := range urls {
+    go hitUrl(url, c)
   }
 
-  word, _ := dictionary.Search(baseWord)
-  fmt.Println(word)
+  for i:=0; i < len(urls); i++{
+    result := <- c
+    results[result.url] = result.status
+  }
+
+  for url, status := range results {
+    fmt.Println(url, status)
+  }
+
 }
+
+func hitUrl(url string, c chan<- requestResult)  {
+  resp, err := http.Get(url)
+  status := "OK"
+  if err != nil || resp.StatusCode >= 400 {
+    status = "FAILED"
+  }
+  c <- requestResult {
+    url:url,
+    status: status,
+  }
+
+}
+
